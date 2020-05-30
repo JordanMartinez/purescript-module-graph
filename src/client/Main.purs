c4734a8125b4c53b7128aaa2a9736d0323ed61fe
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-30 09:15:34 -0700

    Update server to send file content on path "/"; port parser to client

--- src/client/Main.purs
+++ src/client/Main.purs
@@ -2,21 +2,65 @@ module Client.Main where
 
 import Prelude
 
+import Affjax as AX
+import Affjax.ResponseFormat as AXRF
+import Client.Parser (Dependency(..), Module(..), ModuleInfo(..), Path(..), pursGraphOutputParser)
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Data.Monoid (power)
+import Data.String.CodeUnits (drop, take)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Aff (Aff)
+import Effect.Aff (Aff, Milliseconds(..), delay)
+import Halogen (liftAff)
 import Halogen as H
 import Halogen.Aff (awaitBody, runHalogenAff)
 import Halogen.HTML as HH
 import Halogen.Hooks as Hooks
 import Halogen.VDom.Driver (runUI)
+import Text.Parsing.StringParser (ParseError(..), unParser)
 
 main :: Effect Unit
 main = runHalogenAff do
   body <- awaitBody
-  runUI component unit body
+  loadingIO <- runUI loading unit body
 
-component :: forall q i o. H.Component HH.HTML q i o Aff
-component = Hooks.component \_ _ -> do
+  reqResult <- AX.request (AX.defaultRequest { url = "http://localhost:3000/", method = Left GET, responseFormat = AXRF.string })
+  let parseResults = case reqResult of
+        Left err -> Left $
+          "GET http://localhost:3000/ response failed to decode: " <> AX.printError err
+        Right response -> do
+          let text = response.body
+          case unParser pursGraphOutputParser { pos: 0, str: response.body } of
+            Left parseError -> Left $
+              "Text parsed so far: `" <> take parseError.pos text <> "`\n\n\
+              \Parser error at position: " <> show parseError.pos <> "\n\
+              \Error Message: " <> (case parseError.error of ParseError str -> str) <>
+              "\nPrev 30 characters: `" <> (take 30 (drop (parseError.pos - 30) text)) <> "`\n\
+              \nNext 30 characters: `" <> (take 30 (drop parseError.pos text)) <> "`"
+            Right parseResult -> Right parseResult.result
+
+  loadingIO.dispose
+  case parseResults of
+    Left errorMessage -> do
+      runUI displayError errorMessage body
+    Right fileContent -> do
+      runUI displayGraph fileContent body
+
+
+loading :: forall q i o. H.Component HH.HTML q i o Aff
+loading = Hooks.component \_ _ -> Hooks.do
+  state /\ stateId <- Hooks.useState 0
+  Hooks.useLifecycleEffect do
+    id <- Hooks.fork do
+      void $ liftAff do
+        delay (Milliseconds 333.0)
+      Hooks.modify_ stateId (\s -> if s == 3 then 0 else s + 1)
+    pure $ Just do
+      Hooks.kill id
+
+  Hooks.pure $
+    HH.h1_
+      [ HH.text $ "Loading" <> (power "." state) ]
   Hooks.pure $
     HH.div_
-      [ HH.text "it works" ]
--- src/server/Main.purs
+++ src/server/Main.purs
@@ -3,40 +3,29 @@ module Server.Main where
 import Prelude hiding (between)
 
 import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
 import Data.String.CodeUnits (drop, take)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Node.Encoding (Encoding(..))
-import Node.FS.Aff (readTextFile)
-import Network.Wai (responseStr, Application)
-import Network.Warp.Run (runSettings)
-import Network.Warp.Settings (defaultSettings)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (Application, responseFile, responseStr)
+import Network.Warp.Run (runSettings)
+import Network.Warp.Settings (defaultSettings)
+import Node.Encoding (Encoding(..))
+import Node.FS.Aff (readTextFile)
 import Server.Parser (pursGraphOutputParser)
 import Text.Parsing.StringParser (ParseError(..), unParser)
 
 main :: Effect Unit
-main = launchAff_ do
-  file <- readTextFile UTF8 "./module-graph.json"
-  case unParser pursGraphOutputParser { pos: 0, str: file } of
-    Left parseError -> liftEffect $ log $
-      "Text parsed so far: `" <> take parseError.pos file <> "`\n\n\
-      \Parser error at position: " <> show parseError.pos <> "\n\
-      \Error Message: " <> (case parseError.error of ParseError str -> str) <>
-      "\nPrev 30 characters: `" <> (take 30 (drop (parseError.pos - 30) file)) <> "`\n\
-      \nNext 30 characters: `" <> (take 30 (drop parseError.pos file)) <> "`"
-    Right a -> liftEffect $ log $
-      "Succesfully parsed `module-graph.json` content."
-
+main = do
   let beforeMainLoop = do
         log $ "Listening on port " <> show defaultSettings.port
-  void $ liftEffect do
-    runSettings defaultSettings { beforeMainLoop = beforeMainLoop } app
+  void $ runSettings defaultSettings { beforeMainLoop = beforeMainLoop } app
 
 app :: Application
 app req f = do
-    f $ responseStr status200 [(hContentType /\ "text/plain")] "Hello World!"
+    f $ responseFile status200 [(hContentType /\ "text/plain")] "./module-graph.json" Nothing
