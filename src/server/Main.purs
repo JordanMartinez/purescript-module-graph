31dbcfddca995e666669654d6a8be7e2967efe54
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-30 08:23:29 -0700

    Add boilerplate for warp-based server

--- src/server/Main.purs
+++ src/server/Main.purs
@@ -4,12 +4,18 @@ import Prelude hiding (between)
 
 import Data.Either (Either(..))
 import Data.String.CodeUnits (drop, take)
+import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
+import Network.Wai (responseStr, Application)
+import Network.Warp.Run (runSettings)
+import Network.Warp.Settings (defaultSettings)
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
 import Server.Parser (pursGraphOutputParser)
 import Text.Parsing.StringParser (ParseError(..), unParser)
 
@@ -25,3 +31,12 @@ main = launchAff_ do
       \nNext 30 characters: `" <> (take 30 (drop parseError.pos file)) <> "`"
     Right a -> liftEffect $ log $
       "Succesfully parsed `module-graph.json` content."
+
+  let beforeMainLoop = do
+        log $ "Listening on port " <> show defaultSettings.port
+  void $ liftEffect do
+    runSettings defaultSettings { beforeMainLoop = beforeMainLoop } app
+
+app :: Application
+app req f = do
+    f $ responseStr status200 [(hContentType /\ "text/plain")] "Hello World!"
