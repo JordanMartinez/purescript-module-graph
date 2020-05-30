2244a097cc750e99d097fc350e7b510e62eb1d1c
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-30 09:30:08 -0700

    Add 4 routes to server: index.html, app.js, graph file, 404 message

--- src/server/Main.purs
+++ src/server/Main.purs
@@ -3,12 +3,14 @@ module Server.Main where
 import Prelude hiding (between)
 
 import Data.Maybe (Maybe(..))
+import Data.Newtype (un)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Console (log)
-import Network.HTTP.Types (status200)
+import Effect.Unsafe (unsafePerformEffect)
+import Network.HTTP.Types (status200, status404)
 import Network.HTTP.Types.Header (hContentType)
-import Network.Wai (Application, responseFile)
+import Network.Wai (Application, Request(..), responseFile, responseStr)
 import Network.Warp.Run (runSettings)
 import Network.Warp.Settings (defaultSettings)
 
@@ -19,5 +21,15 @@ main = do
   void $ runSettings defaultSettings { beforeMainLoop = beforeMainLoop } app
 
 app :: Application
-app req f = do
+app req f =
+  let _ = unsafePerformEffect $ log $ show (un Request req).pathInfo
+  in case (un Request req).pathInfo of
+  ["", ""] -> do
+    f $ responseFile status200 [(hContentType /\ "text/html")] "./dist/client/index.html" Nothing
+  ["", "app.js"] -> do
+    f $ responseFile status200 [(hContentType /\ "text/javascript")] "./dist/client/app.js" Nothing
+  ["", "api", "graphFile" ] -> do
     f $ responseFile status200 [(hContentType /\ "text/plain")] "./module-graph.json" Nothing
+
+  _ -> do
+    f $ responseStr status404 [(hContentType /\ "text/plain")] "File not found."
