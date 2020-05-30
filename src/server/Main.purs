173803bb264e06881789d780da9e2f8b74dd9b31
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-30 09:17:31 -0700

    Remove unused import files in server

--- src/server/Main.purs
+++ src/server/Main.purs
@@ -2,23 +2,15 @@ module Server.Main where
 
 import Prelude hiding (between)
 
-import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
-import Data.String.CodeUnits (drop, take)
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Aff (launchAff_)
-import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Network.HTTP.Types (status200)
 import Network.HTTP.Types.Header (hContentType)
-import Network.Wai (Application, responseFile, responseStr)
+import Network.Wai (Application, responseFile)
 import Network.Warp.Run (runSettings)
 import Network.Warp.Settings (defaultSettings)
-import Node.Encoding (Encoding(..))
-import Node.FS.Aff (readTextFile)
-import Server.Parser (pursGraphOutputParser)
-import Text.Parsing.StringParser (ParseError(..), unParser)
 
 main :: Effect Unit
 main = do
