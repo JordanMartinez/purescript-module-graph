964e0687284d21455317b3e0c9531e621e4f455a
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-05-31 20:17:36 -0700

    Refactor web server into its own module, App

---
+++ src/Server/App.purs
@@ -0,0 +1,24 @@
+module Server.App where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Maybe (Maybe(..))
+import Data.Tuple.Nested ((/\))
+import Network.HTTP.Types (status200, status404)
+import Network.HTTP.Types.Header (hContentType)
+import Network.Wai (Application, Request(..), responseFile, responseStr)
+import Routing.Duplex (parse)
+import Shared.Routes (PageRoute(..), pageRoutes)
+
+app :: Application
+app (Request req) f = case parse pageRoutes req.rawPathInfo of
+  Right route -> case route of
+    Home -> do
+      f $ responseFile status200 [(hContentType /\ "text/html")] "./dist/index.html" Nothing
+    HalogenFile -> do
+      f $ responseFile status200 [(hContentType /\ "text/javascript")] "./dist/app.js" Nothing
+    _ -> do
+      f $ responseStr status404 [(hContentType /\ "text/plain")] "Not yet implemented."
+  Left _ -> do
+    f $ responseStr status404 [(hContentType /\ "text/plain")] "File not found."
--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -3,24 +3,18 @@ module Server.Main where
 import Prelude hiding (between)
 
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..))
 import Data.String (drop, take)
-import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Network.HTTP.Types (status200, status404)
-import Network.HTTP.Types.Header (hContentType)
-import Network.Wai (Application, Request(..), responseFile, responseStr)
 import Network.Warp.Run (runSettings)
 import Network.Warp.Settings (defaultSettings)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
-import Routing.Duplex (parse)
+import Server.App (app)
 import Shared.Config (baseUrl, port)
 import Shared.Parser (pursGraphOutputParser)
-import Shared.Routes (PageRoute(..), pageRoutes)
 import Text.Parsing.StringParser (ParseError(..), Pos, unParser)
 
 main :: Effect Unit
@@ -37,16 +31,6 @@ main = launchAff_ do
               , port = port
               }
       liftEffect $ void $ runSettings settings app
-
-app :: Application
-app (Request req) f = case parse pageRoutes req.rawPathInfo of
-  Left _ -> do
-    f $ responseStr status404 [(hContentType /\ "text/plain")] "File not found."
-  Right route -> case route of
-    Home -> do
-      f $ responseFile status200 [(hContentType /\ "text/html")] "./dist/index.html" Nothing
-    HalogenFile -> do
-      f $ responseFile status200 [(hContentType /\ "text/javascript")] "./dist/app.js" Nothing
 
 mkErrorMessage :: { error :: ParseError, pos :: Pos } -> String -> String
 mkErrorMessage parseError text =
