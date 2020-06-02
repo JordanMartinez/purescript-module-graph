8e783c6a476ad9608012cac58a497984e38afb9a
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-06-01 20:17:58 -0700

    Implement nonEmptyArrayCodec

--- src/Shared/Codec.purs
+++ src/Shared/Codec.purs
@@ -2,10 +2,12 @@ module Shared.Codec where
 
 import Prelude
 
+import Data.Array.NonEmpty (NonEmptyArray, fromNonEmpty, uncons)
 import Data.Codec.Argonaut (JsonCodec, array, string)
 import Data.Codec.Argonaut as CA
 import Data.Codec.Argonaut.Record as CAR
-import Data.Profunctor (wrapIso)
+import Data.NonEmpty (NonEmpty(..))
+import Data.Profunctor (dimap, wrapIso)
 import Shared.Types (AllInfo(..), Module(..), Package(..), PathToFile(..), Version(..))
 
 moduleCodec :: JsonCodec Module
@@ -29,3 +31,14 @@ allInfoCodec = wrapIso AllInfo $
     , version: versionCodec
     , dependencies: array moduleCodec
     }
+
+nonEmptyArrayCodec :: forall a. JsonCodec a -> JsonCodec (NonEmptyArray a)
+nonEmptyArrayCodec codec =
+    dimap uncons recons record
+  where
+    record = CA.object "NonEmptyArray" $ CAR.record
+      { head: codec
+      , tail: array codec
+      }
+
+    recons { head, tail } = fromNonEmpty (NonEmpty head tail)
