817a094440d0713617b11c4db2b97fc7a900fed8
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-06-03 07:51:26 -0700

    Add typical html helpers

---
+++ src/Client/Utils.purs
@@ -0,0 +1,19 @@
+module Client.Utils where
+
+import Prelude
+
+import Data.Maybe (maybe)
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+
+class_ :: forall p i. String -> HH.IProp (class :: String | i) p
+class_ = HP.class_ <<< HH.ClassName
+
+classes_ :: forall p i. Array String -> HH.IProp (class :: String | i) p
+classes_ = HP.classes <<< map HH.ClassName
+
+whenElem :: forall p i. Boolean -> (Unit -> HH.HTML i p) -> HH.HTML i p
+whenElem cond render = if cond then render unit else HH.text ""
+
+mbElem :: forall p i. Maybe a -> (a -> HH.HTML i p) -> HH.HTML i p
+mbElem mb f = maybe (HH.text "") f mb
